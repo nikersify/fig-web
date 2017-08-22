201d0f2a03d00409c93cbb66300a5e031457d184
nikersify <nikersifyyt@gmail.com>
2017-08-22 17:50:49 +0200

    add a config file and a basic logger

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+const fs = require('fs')
+
 const bodyParser = require('body-parser')
 const express = require('express')
+const log = require('@nikersify/log')
+
+if (!fs.existsSync('config.json')) {
+	log.fatal('app', `copy ${log.e('config.example.json')} ` +
+		`to ${log.e('config.json')} before running the app`)
+
+	throw new Error('couldn\'t read config file')
+}
+
+const config = require('./config')
+
+log.success('app', `read ${log.e('config.json')}`)
 
 const app = express()
 
@@ -9,6 +23,7 @@ app.use(bodyParser.json())
 app.use('/static', express.static('public'))
 app.use(require('./controllers'))
 
-const l = app.listen(3000, () => {
-	console.log(`server listening on port ${l.address().port}`)
+const l = app.listen(config.http.port, () => {
+	log.success('http', 'server started')
+	log.info('http', `server listening on port ${log.e(l.address().port)}`)
 })
